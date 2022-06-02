a8c0393b9df735914b7df6d8b146b9063ac6d4c4
guylamdan <guylamdan@mail.tau.ac.il>
2022-06-02 21:40:21 +0300

    python + bonus

--- bonus.py
+++ bonus.py
@@ -1,25 +1,26 @@
-from turtle import color
-from matplotlib import colors
-import sklearn
 from sklearn.cluster import KMeans
 from sklearn.datasets import load_iris
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-data_set = load_iris(return_X_y=True, as_frame=True)[0]
+data = load_iris(return_X_y=True, as_frame=True)[0]
 interias = []
 
 for i in range (1,11):
     model = KMeans(i, random_state=0)
-    interias.append(model.fit(data_set).inertia_)
+    interias.append(model.fit(data).inertia_)
 
 x_axis = np.array([i for i in range (1,11)])
 y_axis = np.array(interias)
 
 plt.plot(x_axis,y_axis,color = "green", markevery = [1], marker='$◌$', markersize=20)
-plt.annotate("elbow",(2,interias[1]), xytext=(0.4, 0.4), textcoords='axes fraction',
-            arrowprops=dict(arrowstyle="->"))
+
+plt.annotate("elbow",(2,interias[1]),  xycoords='data',
+            xytext=(0.4,0.4), textcoords="axes fraction",
+            arrowprops=dict(width = 0.001, facecolor='black', shrink=0.01),
+            horizontalalignment='right', verticalalignment='top',
+            )
 
 plt.xlabel('K - Values')
 plt.ylabel('Interias')
--- kmeans_pp.py
+++ kmeans_pp.py
@@ -1,39 +1,44 @@
+from mimetypes import init
 import pandas as pd
 import numpy as np
 import sys
 import mykmeanssp
 
+
+
 def calc_distance(vec1, vec2):
-    distance = np.sum(np.power(vec1.subtract(vec2),2)[1:])
-    return (float(distance))
-
-def kmeans_pp (k,data_arr,n,d): 
-    np.random.seed(0)
-    index_of_first_cntrd = np.random.choice(n)
-    index_of_first_cntrd = (int) (merge_data_frame.loc[merge_data_frame[0] == index_of_first_cntrd].index[0])
-    init_indexes = [index_of_first_cntrd] 
-    Dlist = np.full(n, np.inf)
-    Plist = np.zeros((n), dtype=np.float64)  
-    i = 1
-    new_centroid = data_arr.iloc[index_of_first_cntrd]
-    while(i < k):
-        for vecIndex in range(len(data_arr)):
-            vec1 = data_arr.iloc[vecIndex]
-            t_index = (int)(merge_data_frame.loc[vecIndex][0])
-            Dlist[t_index] = min(calc_distance(new_centroid,vec1), Dlist[t_index]) 
-
-        Dtotal = Dlist.sum()
-        for vecIndex in range(len(data_arr)):
-            t_index = (int)(merge_data_frame.loc[vecIndex][0])
-            Plist[t_index] = (Dlist[t_index]/Dtotal)
-        i += 1 
-        chosen_index = np.random.choice(n, size = None, p=Plist)
-        chosen_index = (int) (merge_data_frame.loc[merge_data_frame[0] == chosen_index].index[0])
-        new_centroid = data_arr.iloc[chosen_index] 
-        init_indexes.append(chosen_index)
+    return sum((vec1 - vec2)**2)
     
-    return [int(data_arr.loc[i][0]) for i in init_indexes], pd.DataFrame(data_arr.iloc[[i for i in init_indexes]])
+
+
         
+def kmeans_pp(k,data_arr,n,d):
+    np.random.seed(0)
+    first_centroid = np.random.choice(n)
+
+    centroids_index = []
+    p_list = [0 for i in range(n)] #np.zeros((n), dtype=np.float64)
+    d_list = [np.inf for i in range (n)] #np.full(n, np.inf)
+
+    centroids_index.append(first_centroid)
+    d_sum = 0
+    j = 1
+    while j < k:
+        new_centroid = data_arr.loc[centroids_index[j-1]]
+        for i in range(n):
+            curr_distance = calc_distance(data_arr.loc[i], new_centroid)
+            if curr_distance < d_list[i]:
+                d_list[i] = curr_distance
+        d_sum = sum(d_list)
+
+        for i in range(n):
+            p_list[i] = d_list[i] / d_sum
+        next_centroid = np.random.choice(n, size=None, p=p_list)
+        centroids_index.append(next_centroid)
+        j += 1
+    
+    return centroids_index, data_arr.loc[centroids_index]
+
 def recieve_input():
     arguments_size = len(sys.argv)
     k_float = float(sys.argv[1])
@@ -63,8 +68,6 @@ def validate_input(k,max_iter,n,epsilon):
         exit()
     return
 
-
-
 def check_len(coordinate):
             str_coor = str(coordinate)
             str_coor_array = str_coor.split(".")
@@ -79,24 +82,19 @@ def print_final_centroids(k_centriods):
 
 if __name__ == "__main__":
     k, max_iter,epsilon, input_file_1, input_file_2 = recieve_input() 
-    data_frame1 = pd.read_csv(input_file_1 , header=None)
-    data_frame2 = pd.read_csv(input_file_2 , header=None)
-    merge_data_frame = pd.merge(data_frame1, data_frame2 ,on=0)
-    full_data_points = pd.merge(data_frame1, data_frame2, left_index=True, right_index=True)
+    data_frame1 = pd.read_csv(input_file_1 , header=None, index_col=0)
+    data_frame2 = pd.read_csv(input_file_2 , header=None, index_col=0)
+    merge_data_frame = pd.merge(data_frame1, data_frame2, left_index=True, right_index=True)
     n = len(merge_data_frame) # n = num of rows
-    d = len(merge_data_frame.columns) - 1# d=  num of columns
+    d = len(merge_data_frame.columns)# d=  num of columns
     validate_input(k,max_iter,n,epsilon)
     first_init, first_k = kmeans_pp(k,merge_data_frame, n,d)
-
-    first_k.drop([0], axis=1, inplace=True)
-    merge_data_frame.drop([0], axis=1, inplace=True)
     vectors = merge_data_frame.values.tolist() # DataFrame -> [[],[],[]...]
     k_init_centroids = first_k.values.tolist() 
     k_centroids = mykmeanssp.fit(n, d, k, max_iter, epsilon, vectors, k_init_centroids)
-
     print(",".join([str(i) for i in first_init]))
     print_final_centroids(k_centroids)
-    #python3 kmeans_pp.py 15 750 0 input_3_db_1.txt input_3_db_2.txt
+    #python3 kmeans_pp.py 3 100 0 input_1_db_1.txt input_1_db_2.txt
 
 
     
