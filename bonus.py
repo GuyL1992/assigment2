dcc7308d9af0eed32247fc4ba625aabe21653126
guylamdan <guylamdan@mail.tau.ac.il>
2022-06-03 21:35:34 +0300

    new bonus

--- bonus.py
+++ bonus.py
@@ -7,18 +7,18 @@ import matplotlib.pyplot as plt
 data = load_iris(return_X_y=True, as_frame=True)[0]
 interias = []
 
-for i in range (1,11):
+for i in range (2,11):
     model = KMeans(i, random_state=0)
     interias.append(model.fit(data).inertia_)
 
-x_axis = np.array([i for i in range (1,11)])
+x_axis = np.array([i for i in range (2,11)])
 y_axis = np.array(interias)
 
 plt.plot(x_axis,y_axis,color = "green", markevery = [1], marker='$◌$', markersize=20)
 
-plt.annotate("Elbow Point",(2,interias[1]),  xycoords='data',
-            xytext=(0.4,0.4), textcoords="axes fraction",
-            arrowprops=dict(width = 0.001, facecolor='black', shrink=0.02),
+plt.annotate("Elbow Point",(3,interias[1]),  xycoords='data',
+            xytext=(0.6,0.6), textcoords="axes fraction",
+            arrowprops=dict(width = 0.001, facecolor='black',),
             horizontalalignment='right', verticalalignment='top',
             )
 
